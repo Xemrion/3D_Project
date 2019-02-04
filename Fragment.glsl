f2bdf4feeadafdda7ed45c96dd2bf575a20a6f2a
Xemrion <38206179+Xemrion@users.noreply.github.com>
2019-02-04 09:41:01 +0100

    SOmething

--- Fragment.glsl
+++ Fragment.glsl
@@ -13,19 +13,33 @@ uniform sampler2D texture_sampler;
 
 #//////////////////////////////////////////////////////////
 
-vec3 lightpos = vec3(0.0f, 2.0f, 3.0f);
-vec3 ambient = vec3(1.0f, 1.0f, 1.0f) * 0.1;
+vec3 lightpos = vec3(0.0f, 1.0f, 4.0f);
+vec3 ambient = vec3(1.0f, 1.0f, 1.0f) * 0.1f;
+
+
 
 vec3 normal = normalize(Normal);
 vec3 lightDir = normalize(lightpos-fragPos);
 float diff = max(dot(normal, lightDir), 0.0);
 vec3 diffuse = diff*vec3(1.0f, 1.0f, 1.0f);
 
-vec3 light = ambient+diffuse;
+
+
+vec3 cameraPos = vec3(2, 2, 4);
+float materialShininess = 5.0f;
+
+vec3 reflectionVector = reflect(-lightDir,normal);
+vec3 surfaceToCamera = normalize(cameraPos - fragPos);
+float cosAngle = max(0.0, dot(surfaceToCamera, reflectionVector));
+float specularCoefficient = pow(cosAngle, materialShininess);
+vec3 specular = specularCoefficient*  vec3(0.5f, 0.5f, 0.5f)  *vec3(1.0f, 1.0f, 1.0f);
+
+
+
+vec3 light = ambient*0.5f+diffuse*0.5f+specular*3.0f;
 #//////////////////////////////////////////////////////////
 
 void main () 
 {
-	
 	fragment_color = vec4(frag_color, 1.0f) * vec4(light,1.0);
 }
